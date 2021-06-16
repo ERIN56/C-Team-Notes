720e9762f3f9d8e81d9a3561fa1f311371349411
ERIN56 <83942393+ERIN56@users.noreply.github.com>
2021-06-16 16:25:40 +0900

    Update InsertionSort.cpp

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,4 +1,4 @@
-삽입 정렬(O(n^2), 최선 : O(n))
+삽입 정렬(O(n^2), 최선 : O(n)-데이터가 거의 정렬되어 있을 때)
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,6 +8,7 @@ int arr[10] = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
 
 int main(void) {
     for (int i = 1; i < n; i++) {
+        
         // 인덱스 i부터 1까지 감소하며 반복하는 문법
         for (int j = i; j > 0; j--) {
             // 한 칸씩 왼쪽으로 이동
@@ -22,3 +23,6 @@ int main(void) {
         cout << arr[i] << ' ';
     }
 }
+
+<출력 결과>
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9
