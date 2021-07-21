45c62e4997b251a3a9063486e78d19dde33af205
ERIN56 <83942393+ERIN56@users.noreply.github.com>
2021-07-21 15:46:01 +0900

    Create map<string, vector> map.cpp

---
+++ STL/miscellaneous/map<string, vector> map.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    
+    map<string, vector<string>> mCloth;
+  
+    for(auto elem : clothes)
+    {
+        mCloth[elem.at(1)].push_back(elem.at(0));
+    }
+  
+    for(auto elem : mCloth)
+    {
+        answer*=elem.second.size()+1;
+    }
+  
+    answer--;
+    return answer;
+}
+
+<입력 예시>
+
+[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+[["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+
+<map 결과>
+
+["headgear"] : ["yellowhat", "green_turban"]
+["eyewear"] : ["bluesunglasses"]
+-------------------------
+["face"] : ["crowmask", "bluesunglasses", "smoky_makeup"];
+
+
+출처 : 프로그래머스 > level2 > 위장 https://programmers.co.kr/learn/courses/30/lessons/42578
