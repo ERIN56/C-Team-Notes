1144529a82ffa1685cfa70c7b9cdf72ef526fe19
LeeJuHee <83942393+ERIN56@users.noreply.github.com>
2021-10-18 20:29:51 +0900

    Create tuple.cpp

---
+++ STL/tuple/tuple.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+int main(){
+ 
+  tuple<int, char> foo (10, 'x');
+  auto bar = make_tuple ("test", 3.1, 14, 'y');
+  
+  get<2>(bar) = 100;      // access element
+  
+  int myint;
+  char mychar;
+  
+  tie(myint, mychar) = foo;     // unpack element
+  tie(ignore, ignore, myint, mychar) = bar;
+  
+  mychar = get<3>(bar);
+  
+  cout<<"foo contains: ";
+  cout<<get<0>(foo)<<' ';
+  cout<<get<1>(foo)<<'\n';
+  
+  //////////////////////////////////////////////////////////////////////
+  
+  queue<tuple<int, int, int>> que;
+  que.push({1, 2, 3});
+  que.push(make_tuple(1, 2, 3));
+  
+  int z = get<0>(que.front());
+  
+           
+}
